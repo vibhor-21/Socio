0da0799893f7733b50c117b11762226368baeab0
Vibhor Kumar <vibhor@Vibhors-MacBook-Air.local>
2021-09-21 16:39:06 +0530

    added comment section

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
     // res.cookies, res.cookie('user_id;,25);
-    return res.render('home',{
-        title: "Home"
-    });
+    // return res.render('home',{
+    //     title: "Home"
+    // });
+ // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
 }
