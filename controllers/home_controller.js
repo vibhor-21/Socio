2926a154bb223834d4470293d755a35bc9bfb645
Vibhor Kumar <vibhor@Vibhors-MacBook-Air.local>
2021-10-06 10:03:08 +0530

    updated controller and config

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,37 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // res.cookies, res.cookie('user_id;,25);
-    // return res.render('home',{
-    //     title: "Home"
-    // });
- // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+
+
+module.exports.home = async function(req, res){
+
+    try{
+        // CHANGE :: populate the likes of each post and comment
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            },
+            populate: {
+                path: 'likes'
+            }
+        }).populate('likes');
+
+    
+        let users = await User.find({});
+
         return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+            title: "Socio | Home",
+            posts:  posts,
+            all_users: users
         });
-    })
 
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+   
 }
